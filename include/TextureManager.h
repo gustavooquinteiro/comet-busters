f28c861f6b6763ac3971f28dca793a8cdc387dc3
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-07 17:58:40 -0300

    Add TextureManager

---
+++ include/TextureManager.h
@@ -0,0 +1,21 @@
+#ifndef _TEXTURE_MANAGER_H_
+#define _TEXTURE_MANAGER_H_
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <map>
+
+
+using namespace std;
+
+class TextureManager
+{
+public:
+    void draw(string id, int x, int y, int width, int height, SDL_Renderer* renderer, SDL_RendererFlip flip);
+    bool load(string filename, string id, SDL_Renderer* renderer); 
+private:
+    TextureManager();
+    map<string, SDL_Texture*> textureMap;
+};
+
+#endif
---
+++ src/TextureManager.cpp
@@ -0,0 +1,35 @@
+#include "../include/TextureManager.h"
+
+bool TextureManager::load(string filename, string id, SDL_Renderer* renderer)
+{
+    SDL_Surface* temp = IMG_Load(filename.c_str());
+    
+    if (temp == 0)
+        return false;
+ 
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, temp);
+    SDL_FreeSurface(temp);
+    
+    if (texture)
+    {
+        textureMap[id] = texture;
+        return true;
+    }
+    return false;
+}
+
+void TextureManager::draw(string id, int x, int y, int width, int height, SDL_Renderer* renderer, SDL_RendererFlip flip)
+{
+   SDL_Rect srcRect;
+   SDL_Rect dstRect;
+   
+   srcRect.x = 0;
+   srcRect.y = 0;
+   srcRect.w = dstRect.w = width;
+   srcRect.h = dstRect.h = height;
+   
+   dstRect.x = x;
+   dstRect.y = y;
+   
+   SDL_RenderCopyEx(renderer, textureMap[id], &srcRect, &dstRect, 0, 0, flip);
+}
