b9a6c1278565e0d32ecc8dfac30c7cbb83a4524c
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-07 16:34:23 -0300

    Implement init method

--- include/Game.h
+++ include/Game.h
@@ -8,7 +8,7 @@ public:
     Game();
     ~Game();
     
-    bool init();
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
     void render();
     void update();
     void handleEvents();
--- src/Game.cpp
+++ src/Game.cpp
@@ -4,7 +4,32 @@ Game::Game(){}
 
 Game::~Game(){}
 
-bool Game::init(){ return true; }
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        if (window)
+        {
+            renderer = SDL_CreateRenderer(window, -1, 0);
+            if (renderer)
+            {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            } else
+            {
+                return false;
+            }
+        } else
+        {
+            return false;
+        }
+    } else
+    {
+        return false;
+    }
+    run = true;
+    return true;
+}
 void Game::render(){}
 void Game::update(){}
 void Game::handleEvents(){}
