1caf6ebe65d9d770586d623e40ababbb25e1cbf3
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-08 07:04:59 -0300

    Implement base class methods

--- include/Player.h
+++ include/Player.h
@@ -1,11 +1,16 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
+#include <SDL2/SDL.h>
 #include "GameObject.h"
 
 class Player: public GameObject
 {
 public:
+    void load(int x, int y, int width, int height, std::string textureId);
+    void draw(SDL_Renderer* renderer);
+    void update();
+    void clean();
     long int getScore();
 private:
     long int score;
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,3 +1,22 @@
 #include "../include/Player.h"
 
+void Player::load(int x, int y, int width, int height, std::string textureId)
+{
+    GameObject(x, y, width, height, textureId);
+}
+
+void Player::draw(SDL_Renderer* renderer)
+{
+    GameObject::draw(renderer);
+}
+
+void Player::update()
+{
+}
+
+void Player::clean()
+{
+    GameObject::clean();
+}
+
 long int Player::getScore(){ return this->score; }
