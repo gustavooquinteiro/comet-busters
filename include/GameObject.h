39caefa3b03dafebaeeb5cd05164b5ebcc535b01
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-08 07:02:05 -0300

    Add and implement methods

--- include/GameObject.h
+++ include/GameObject.h
@@ -1,15 +1,25 @@
 #ifndef _GAME_OBJECT_H_
 #define _GAME_OBJECT_H_
 
+#include <SDL2/SDL.h>
+#include <iostream>
+using namespace std;
+
 class GameObject
 {
 public:
-    void draw();
+    GameObject(int x, int y, int width, int height, string textureId);
+    void load();
+    void draw(SDL_Renderer* renderer);
     void update();
     void clean();
 protected:
     int x;
     int y;
+    int width;
+    int height;
+    
+    string textureId;
 };
 
 #endif
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,11 +1,16 @@
 #include "../include/GameObject.h"
+#include "../include/TextureManager.h"
 
-void GameObject::draw()
+GameObject::GameObject(int x, int y, int width, int height, std::string textureId): x(x), y(y), width(width), height(height), textureId(textureId){}
+
+void GameObject::draw(SDL_Renderer* renderer)
 {
+    TextureManager::Instance()->draw(textureId, x, y, width, height, renderer);    
 }
 
 void GameObject::update()
 {
+    x++;
 }
 
 void GameObject::clean()
