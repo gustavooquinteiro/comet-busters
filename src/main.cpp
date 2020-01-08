4a01cac0339e3ead00d4b157301a2a2bc6b76312
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-08 13:35:50 -0300

    Create fixed frames per second

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include "../include/Game.h"
+
 const char* TITLE = "Comet Busters! 1994";
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* argv[])
 {
+    Uint32 frameStart, frameTime;
+    
     Game* game = new Game();
     game->init(TITLE, 100, 100, 640, 480, 0);
     
     while(game->running())
     {
+        frameStart = SDL_GetTicks();
+        
         game->handleEvents();
         game->update();
         game->render();
         
-        SDL_Delay(10);
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < DELAY_TIME)
+            SDL_Delay((int) (DELAY_TIME - frameTime));
     }
     
     game->clean();
