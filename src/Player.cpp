686a8c9239415e286a7d6dca247d5adde03b3963
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-08 08:11:06 -0300

    Fix constructor implementation

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,9 +1,6 @@
 #include "../include/Player.h"
 
-Player::Player(int x, int y, int width, int height, std::string textureId)
-{
-    GameObject(x, y, width, height, textureId);
-}
+Player::Player(int x, int y, int width, int height, std::string textureId): GameObject(x, y, width, height, textureId) {}
 
 void Player::load()
 {
