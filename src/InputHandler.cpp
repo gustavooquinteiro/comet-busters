b6b37bd457769fd4c97f9f663029a9438218ec11
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-08 15:29:23 -0300

    Add keyboard handler skeleton

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,4 +1,6 @@
 #include "../include/InputHandler.h"
+#include <iostream>
+using namespace std;
 
 InputHandler* InputHandler::instance = 0;
 
@@ -14,6 +16,22 @@ InputHandler* InputHandler::Instance()
 
 void InputHandler::update()
 {
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_KEYUP:
+                break;
+            case SDL_KEYDOWN:
+                break;
+            case SDL_QUIT:
+                break;
+            default:
+                break;
+                
+        }
+    }
 }
 
 void InputHandler::clean()
