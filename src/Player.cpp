04e9af70eed8d6b87dc577827fb5cb33eaedbb20
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-09 10:00:01 -0300

    Add player movement scheme

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,4 +1,5 @@
 #include "../include/Player.h"
+#include "../include/InputHandler.h"
 
 Player::Player(int x, int y, int width, int height, std::string textureId): GameObject(x, y, width, height, textureId) {}
 
@@ -14,6 +15,13 @@ void Player::draw(SDL_Renderer* renderer)
 
 void Player::update()
 {
+    if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
+        velocity.setX(2);
+    if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
+        velocity.setX(-2);
+    if (InputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
+        velocity.setX(-2);
+    
 }
 
 void Player::clean()
