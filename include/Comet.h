8f74604c828904ee012cdd95eb326683fb709745
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-10 10:44:53 -0300

    Add LoaderParams as a constructor parameter

--- include/Comet.h
+++ include/Comet.h
@@ -4,14 +4,14 @@
 #include <SDL2/SDL.h>
 #include "GameObject.h"
 
-class Comet: public GameObject
+class Comet: public SDLGameObject
 {
 public:
-    Comet(int x, int y, int width, int height, std::string textureId);
-    void load();
-    void draw(SDL_Renderer* renderer);
-    void update();
-    void clean();
+    Comet(const LoaderParams* params);
+    virtual void load();
+    virtual void draw();
+    virtual void update();
+    virtual void clean();
 };
 
 #endif
