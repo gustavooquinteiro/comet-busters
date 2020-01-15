e58dc66d7103936ff8b1993bf1e21b95b08f667e
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-15 18:55:24 -0300

    Add onEnter and onExit implementation on MainMenuState

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -1,4 +1,6 @@
 #include "../include/MenuState.h"
+#include "../include/StateParser.h"
+#include "../include/TextureManager.h"
 
 const string MenuState::menuID = "MENU";
 
@@ -24,4 +26,38 @@ void MenuState::update()
 
 string MenuState::getStateID() const{ return this->menuID; }
 
+
+string MainMenuState::getStateID() const{ return this->menuID; }
+
+bool MainMenuState::onEnter()
+{
+    StateParser stateparser;
+    stateparser.parseState("test.xml", menuID, &gameObjects, &textureIDList);
+    callbacks.push_back(0);
+    callbacks.push_back(menuToPlay);
+    callbacks.push_back(exitFromMenu);
+    
+    setCallbacks(callbacks);
+    return true;
+}
+
+void MainMenuState::setCallbacks(const vector<MenuState::Callback>& callbacks)
+{
+    for(auto gameObject: gameObjects)
+    {
+        if (dynamic_cast<MenuButton*>(gameObject))
+        {
+            MenuButton* button = dynamic_cast<MenuButton*>(gameObject);
+            button->setCallback(callbacks[button->getCallbackID()]);
+        }
+    }
+}
+
+bool MainMenuState::onExit()
+{
+    for(auto textureIDs: textureIDList)
+    {
+        TextureManager::Instance()->clearFromTextureMap(textureIDs);
+    }
+}
 
