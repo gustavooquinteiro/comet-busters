c7832e6c17d92e162b02a75d13db7831e1163864
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-16 10:04:01 -0300

    Add update method implementation

--- src/MenuButton.cpp
+++ src/MenuButton.cpp
@@ -1,10 +1,32 @@
 #include "../include/MenuButton.h"
+#include "../include/InputHandler.h"
 
 MenuButton::MenuButton(void (*callback)()): callback(callback) {}
 
 void MenuButton::update()
 {
-    //TODO: update function following SDL book on page 115
+    Vector2D* mousePosition = InputHandler::Instance()->getMousePosition();
+    if (mousePosition->getX() < (position.getX() + width) && mousePosition->getX() > position.getX())
+    {
+        if (mousePosition->getY() < (position.getY() + height) && mousePosition->getY() > position.getY())
+        {
+            if (InputHandler::Instance()->getMouseButtonState(LEFT) && buttonReleased)
+            {
+                currentFrame = CLICKED;
+                callback();
+                buttonReleased = false;
+            }
+            else if (!InputHandler::Instance()->getMouseButtonState(LEFT))
+            {
+                buttonReleased = true;
+                currentFrame = MOUSE_OVER; 
+            }
+            else
+            {
+                currentFrame = MOUSE_OUT;
+            }  
+        }
+    }
 }
 
 void MenuButton::clean()
