53964c9ef4ae3664c6eae41ce8fa5f727e528eb0
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-16 11:27:34 -0300

    Add PauseState class method implementation

---
+++ src/PauseState.cpp
@@ -0,0 +1,50 @@
+#include "../include/InputHandler.h"
+#include "../include/PauseState.h"
+#include "../include/MenuState.h"
+#include "../include/Game.h"
+
+const string PauseState::pauseID = "PAUSE";
+
+void PauseState::pauseToMain()
+{
+    Game::Instance()->getStateMachine()->changeState(new MenuState());
+}
+
+void PauseState::resumePlay()
+{
+    Game::Instance()->getStateMachine()->popState();
+}
+
+void PauseState::update()
+{
+    for (auto object: gameObjects)
+        object->update();
+}
+
+void PauseState::render()
+{
+    for (auto object: gameObjects)
+        object->draw();
+}
+
+bool PauseState::onEnter()
+{
+    return false;
+}
+
+bool PauseState::onExit()
+{
+    for (auto object: gameObjects)
+        object->clean();
+    
+    gameObjects.clear();
+    
+    for (auto texture: textureIDList)
+        TextureManager::Instance()->clearFromTextureMap(texture);
+    
+    InputHandler::Instance()->reset();
+    return true;
+}
+
+
+string PauseState::getStateID() const{ return this->pauseID; }
