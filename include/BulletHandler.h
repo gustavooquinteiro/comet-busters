a29da24957889f0f6d0e873d27d7603e22843986
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-01-22 09:32:55 -0300

    Add bullet handler class

---
+++ include/BulletHandler.h
@@ -0,0 +1,27 @@
+#ifndef _BULLET_HANDLER_H_
+#define _BULLET_HANDLER_H_
+
+#include "Vector2D.h"
+#include "Bullet.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class BulletHandler
+{
+public:
+    void addPlayerBullet(int x, int y, int width, int height, string textureID, int numFrames, Vector2D heading);
+    void addEnemyBullet(int x, int y, int width, int height, string textureID, int numFrames, Vector2D heading);
+    static BulletHandler* Instance();
+
+    vector<PlayerBullet*> getPlayerBullets() const;
+    vector<EnemyBullet*> getEnemyBullets() const;
+private:
+    BulletHandler();
+    static BulletHandler* instance;
+    
+    vector<PlayerBullet*> playerBullets;
+    vector<EnemyBullet*> enemmyBullets;
+}
+
+#endif
