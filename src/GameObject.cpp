ec7c2cd902a6c56d1661767f481477d2dc553832
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-02-02 09:47:19 -0300

    Implement and refactor class methods

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -2,28 +2,37 @@
 #include "../include/GameObject.h"
 #include "../include/TextureManager.h"
 
-GameObject::~GameObject() {}
+GameObject::~GameObject(){}
 
-GameObject::GameObject(): position(0, 0), velocity(0, 0), acceleration(0, 0), width(0), height(0), currentRow(0), currentFrame(0), isDead(false) {}
+GameObject::GameObject(){}
 
-ShooterObject::ShooterObject(): GameObject() {}
+ShooterObject::ShooterObject(): GameObject(),
+                                bulletFiringSpeed(0),
+                                bulletCounter(0),
+                                moveSpeed(0),
+                                dyingTime(0),
+                                dyingCounter(0),
+                                playedDeathSound(false){}
 
 void ShooterObject::draw()
 {
-    TextureManager::Instance()->draw(textureId, (int)position.getX(), (int)position.getY(), width, height, Game::Instance()->getRenderer());
+    TextureManager::Instance()->draw(textureId, 
+                                     (Uint32) position.getX(), 
+                                     (Uint32) position.getY(),
+                                     width, 
+                                     height,
+                                     Game::Instance()->getRenderer());
 }
 
 void ShooterObject::update()
 {
-    velocity += acceleration;
     position += velocity;
 }
 
 void ShooterObject::load(unique_ptr<LoaderParams> const &params)
 {
     position = Vector2D(params->getX(), params->getY());
-    velocity = Vector2D(0, 0);
-    acceleration = Vector2D(0, 0);
+    
     width = params->getWidth();
     height = params->getHeight();
     textureId = params->getTextureID();
@@ -41,4 +50,11 @@ ShooterObject::~ShooterObject()
 
 void ShooterObject::collision()
 {
+}
+
+void ShooterObject::doDyingAnimation()
+{
+    if (dyingCounter == dyingTime)
+        dead = true;
+    dyingCounter++;
 }
