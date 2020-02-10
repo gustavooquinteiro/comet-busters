6c0e8e7ae9d18663818388402af8aca9618d83bb
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-02-10 17:08:31 -0300

    Refactor methods to correct parent class

--- src/UFO.cpp
+++ src/UFO.cpp
@@ -1,5 +1,4 @@
 #include "../include/UFO.h"
-#include "../include/ScreenHandler.h"
 #include "../include/BulletHandler.h"
 
 UFO::UFO(): Enemy()
@@ -10,40 +9,8 @@ UFO::UFO(): Enemy()
 
 void UFO::update()
 {
-    if (!isDead())
-    {
-        static int xShift = moveSpeed;
-        static int yShift = moveSpeed;
-
-        if (!isInScreen(position))
-        {
-            if (isOutOfHeight(position) && isOutOfWidth(position))
-            {
-                xShift = -xShift + 1;
-                yShift = -yShift + 1;
-            }
-            else if (isOutOfWidth(position))
-            {
-                xShift = -xShift;
-            }
-            else if (isOutOfHeight(position))
-            {
-                yShift = -yShift;
-            }
-        }
-        
-        velocity.setX(xShift);
-        velocity.setY(yShift);
-        
-        shoot();
-    } 
-    else
-    {
-        velocity.setX(0);
-        velocity.setY(0);
-        // TODO: Dying animation
-    }
-    ShooterObject::update();
+    Enemy::update();
+    shoot();
 }
 
 void UFO::shoot()
@@ -53,18 +20,18 @@ void UFO::shoot()
     {
         Vector2D heading = velocity;
         BulletHandler::Instance()->addEnemyBullet(position.getX(), position.getY(), 
-                                                  16, 16, "bullet3", 1, heading);
+                                                  33, 33, "bullet3", 1, heading);
         counter = moveSpeed * 100;
     }
     counter--;
 }
 
+void UFO::draw()
+{
+    Enemy::draw();
+}
+
 void UFO::collision()
 {
-    health -= 1;
-    if (health == 0)
-    {
-        // TODO: Dying animation
-        delete this;
-    }
+    Enemy::collision();
 }
