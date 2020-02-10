f98c247955970f6fa4e04bc70d808ba088d145f6
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-02-10 17:04:16 -0300

    Add conversor from GameObject to SDL_Rect method

--- include/Collision.h
+++ include/Collision.h
@@ -1,8 +1,20 @@
 #ifndef _COLLISION_
 #define _COLLISION_
+
 #include <SDL2/SDL.h>
+#include "GameObject.h"
 
 static const int collisionBuffer = 4;
+
+static SDL_Rect* GameObjectToSDLRect(GameObject* object)
+{
+    SDL_Rect* rect = new SDL_Rect();
+    rect->x = object->getPosition().getX();
+    rect->y = object->getPosition().getY();
+    rect->h = object->getHeight();
+    rect->w = object->getWidth();
+    return rect;
+}
 
 static bool RectRect(SDL_Rect* A, SDL_Rect* B)
 {
